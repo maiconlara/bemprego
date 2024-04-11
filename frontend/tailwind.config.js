3dfa98c57ba5833ea0f944f41fb90cad47355c7c
maiconlara <maiconluan.lara@yahoo.com.br>
2024-04-11 10:57:17 -0300

    docs: config shadcn/ui

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,71 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
+  darkMode: ["class"],
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
+  prefix: "",
   theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    screens: {
+      sm: "640px",
 
+      md: "768px",
+
+      lg: "1024px",
+
+      xl: "1280px",
+
+      "2xl": "1536px",
+
+      "3xl": "1600px",
+
+      "4xl": "2560px",
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        nunito: ["Nunito", "sans-serif"],
+      },
+      colors: {
+        ribbon: {
+          50: "#eff4ff",
+          100: "#dbe6fe",
+          200: "#bfd3fe",
+          300: "#93b4fd",
+          400: "#6090fa",
+          500: "#3b76f6",
+          600: "#2563eb",
+          700: "#1d58d8",
+          800: "#1e4baf",
+          900: "#1e408a",
+          950: "#172a54",
+        },
+      },
+
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};

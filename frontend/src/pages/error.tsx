57e4a8e27e36613bb0020d7dd9137c486178012a
maiconlara <maiconluan.lara@yahoo.com.br>
2024-04-11 09:54:07 -0300

    feat: adding pages

---
+++ frontend/src/pages/error.tsx
@@ -0,0 +1,10 @@
+const Error = () => {
+    return (
+      <div id="error-page" className="flex flex-col flex-1 justify-center items-center gap-2 ">
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p></p>
+      </div>
+    );
+  };
+  export default Error;
---
+++ frontend/src/pages/landing-page.tsx
@@ -0,0 +1,12 @@
+import { useTabTitle } from "../utils/hooks/useTabTitle";
+
+const Home = () => {
+  useTabTitle("Home");
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full pb-12">
+    
+    </div>
+  );
+};
+export default Home;

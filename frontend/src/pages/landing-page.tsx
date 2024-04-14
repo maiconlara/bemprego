0137acde03d92e3b8874a547207cbf10d0d837ec
Maicon Lara <maiconluan.lara@yahoo.com.br>
2024-04-14 20:38:10 -0300

    refactor: adding plan component to lp

--- frontend/src/pages/landing-page.tsx
+++ frontend/src/pages/landing-page.tsx
@@ -1,14 +1,16 @@
 import Hero from "@/components/hero";
 import { useTabTitle } from "../utils/hooks/useTabTitle";
 import RotativeBanner from "@/components/rotative-banner";
+import Plans from "@/components/plans";
 
 const Home = () => {
   useTabTitle("Home");
 
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full gap-28">
-    <Hero />
-    <RotativeBanner />
+    <div className="flex flex-col items-center justify-center w-full h-full gap-40 pb-24">
+      <Hero />
+      <RotativeBanner />
+      <Plans />
     </div>
   );
 };

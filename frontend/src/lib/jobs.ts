bb6448fc702f1cc6cb3aa0609ac5bf1b9b1d9deb
Maicon Lara <maiconluan.lara@yahoo.com.br>
2024-04-14 22:34:16 -0300

    feat: job object

---
+++ frontend/src/lib/jobs.ts
@@ -0,0 +1,27 @@
+interface JobItem {
+  key: string;
+  value: string;
+}
+
+export const jobs: JobItem[] = [
+  {
+    key: "frontend developer",
+    value: "Desenvolvedor Front-end",
+  },
+  {
+    key: "backend developer",
+    value: "Desenvolvedor Back-end",
+  },
+  {
+    key: "fullstack developer",
+    value: "Desenvolvedor Fullstack",
+  },
+  {
+    key: "devops",
+    value: "DevOps",
+  },
+  {
+    key: "dataanalyst",
+    value: "Analista de Dados",
+  },
+];
